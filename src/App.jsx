4d87b6ed5b2078c24bb179a2e42bd2639809097e
Akshata-M09 <89059087+Akshata-M09@users.noreply.github.com>
2025-03-26 17:42:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import { useState } from 'react'
+import './App.css'
+import BooksTable from './components/BooksTable';
 
-import "./style.css";
-import Main from "./main";
+const books = [
+  {
+    id: "1",
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    publishedDate: "1960",
+    publisher: "J.B. Lippincott & Co.",
+  },
+  {
+    id: "2",
+    title: "1984",
+    author: "George Orwell",
+    publishedDate: "1949",
+    publisher: "Secker & Warburg",
+  },
+  {
+    id: "3",
+    title: "Moby-Dick",
+    author: "Herman Melville",
+    publishedDate: "1851",
+    publisher: "Harper & Brothers",
+  },
+  {
+    id: "4",
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    publishedDate: "1813",
+    publisher: "T. Egerton",
+  },
+];
 
 function App() {
+  const [bookData, ] = useState(books);
+
   return (
-    <>
-      <Main />
-    </>
-  );
+      <>
+        <BooksTable books={bookData} />
+      </>
+    
+  )
 }
 
-export default App;
+export default App
